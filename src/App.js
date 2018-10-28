a11e199fcccf5281b1686a3d118a28d6a90f1381
panshoku <pan.shoku.mochi@gmail.com>
2018-10-28 21:19:51 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div>!!!! <code>src/App.js</code> and save to reload.</div>
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <p>
-            !!!! <code>src/App.js</code> and save to reload.
-          </p>
+    return <hi>hello</hi>
   }
 }
 
