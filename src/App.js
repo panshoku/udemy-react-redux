da6ef4cdb8b194ff06d4e0dd748dc10f9b6bd023
panshoku <pan.shoku.mochi@gmail.com>
2018-10-28 22:27:48 +0900

    learn prop types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 // 関数コンポーネント
 const App =() => {
     const profiles =[
         { name: "taro", age: 10},
         { name: "hana", age: 11},
-        { name: "hana"},
+        { name: "no"},
     ]
     return (
         <div>
         {
             // 配列.map()：全ての要素を呼び出す。ループ的な
             // 配列.map(x => x*2) : 配列に入ってる要素がxに渡されて=>の処理をする
-            profiles.map((profile) => {
-                return <User name={profile.name} age={profile.age} />
+            profiles.map((profile, index) => {
+                return <User name={profile.name} age={profile.age} key={index}/>
             })
         }
             {/*
@@ -29,6 +30,12 @@ const App =() => {
 const User = (props) => {
     return (<p>i am {props.name}, {props.age}</p>)
 }
+
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
+}
+
 User.defaultProps = {
     age: 1
 }
