e4efe8d711d00894d8dcc916479fd9479f35e745
panshoku <pan.shoku.mochi@gmail.com>
2018-10-28 22:47:57 +0900

    implement action

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// アクションはオブジェクト：キーと値
+// アクションを関数にする
+
+
+export const INCRE = 'INCRE'
+export const DECRE = 'DECRE'
+
+
+export const incre = () => ({
+    type: INCRE
+})
+
+export const incre = () => ({
+    type: DECRE
+})
